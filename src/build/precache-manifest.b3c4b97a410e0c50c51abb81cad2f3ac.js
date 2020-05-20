a571fad634b998931c9cd24bef425b6c5ee991d3
jinsjob <39972022+jinsjob@users.noreply.github.com>
2020-05-20 22:36:48 +1200

    First commit

---
+++ src/build/precache-manifest.b3c4b97a410e0c50c51abb81cad2f3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311854680e3702a53dca159ce077e83e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630ccaf4353b4a474c0b",
+    "url": "/static/css/main.7a67f66c.chunk.css"
+  },
+  {
+    "revision": "84603e2bdce7f1592e65",
+    "url": "/static/js/2.036c5c9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.036c5c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630ccaf4353b4a474c0b",
+    "url": "/static/js/main.18b5db20.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
